8e13fb859c6846bae902afb09d4422dab26aa80d
sola <solaocodes@gmail.com>
2020-01-31 18:01:06 -0500

    Initial commit

---
+++ src/DirectoryContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class DirectoryContainer extends Component {
+  render() {
+    return (
+      <p>Empty!!!</p>
+    );
+  }
+}
+
+
+export default DirectoryContainer;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DirectoryContainer from "./DirectoryContainer";
+
+
+ReactDOM.render(
+    <DirectoryContainer/>, 
+    document.getElementById('root')
+);
