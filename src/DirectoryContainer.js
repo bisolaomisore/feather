11be5d4d2adc337b094c79f0731d7d47184b2db3
sola <solaocodes@gmail.com>
2020-01-31 21:21:43 -0500

    Add ability to fetch user data from Github

--- src/DirectoryContainer.js
+++ src/DirectoryContainer.js
@@ -1,6 +1,35 @@
 import React, { Component } from "react";
 
+
+const config = require("./config");
+const Octokit = require("@octokit/rest");
+const octokit = new Octokit({
+  auth: config.PERSONAL_ACCESS_TOKEN
+});
+
+
 class DirectoryContainer extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      user: {},
+      followers: []
+    };
+  }
+
+  componentDidMount() {
+    Promise.all([
+      octokit.users.getAuthenticated(),
+      octokit.users.listFollowersForAuthenticatedUser(),
+    ]).then(([user, followers]) => {
+      this.setState({
+        user: user.data,
+        followers: followers.data
+      });
+    })
+  }
+
   render() {
     return (
       <p>Empty!!!</p>
---
+++ src/config.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+
+module.exports = {
+  PERSONAL_ACCESS_TOKEN: process.env.REACT_APP_PERSONAL_ACCESS_TOKEN
+}
