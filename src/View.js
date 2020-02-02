3fb3d506a9bd95bc6b7cabc0a3f602ca1787d420
sola <solaocodes@gmail.com>
2020-02-02 14:41:36 -0500

    Add ability to edit Bio

--- src/View.js
+++ src/View.js
@@ -1,9 +1,28 @@
 import React, { useState, useEffect } from "react";
+import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import "./view.css"
 
+const config = require("./config");
+const Octokit = require("@octokit/rest");
+const octokit = new Octokit({
+  auth: config.PERSONAL_ACCESS_TOKEN
+});
+
 function VerticallyCenteredModal(props) {
+  const [userBio, setUserBio] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    octokit.users.updateAuthenticated({
+      bio: userBio
+    })
+  }
+
+  useEffect(() => setUserBio(props.userdata.bio), [props.userdata]);
+
   return (
     <Modal
       {...props}
@@ -17,13 +36,28 @@ function VerticallyCenteredModal(props) {
           <span className="username">
             @{props.userdata.login}
           </span>
+          <Badge variant="success">{props.isauthenticated}</Badge>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <h4>Bio</h4>
         <p>
-          {props.userdata.bio ? props.userdata.bio : "This user has no bio"}
+          {props.userdata.bio ? userBio : "This user has no bio"}
         </p>
+        {props.isauthenticated &&
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBioText">
+              <Form.Control
+                type="text"
+                value={userBio}
+                onChange={e => setUserBio(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+            Submit
+            </Button>
+          </Form>
+        }
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -35,6 +69,7 @@ function VerticallyCenteredModal(props) {
 function View(props) {
   const [modalShow, setModalShow] = useState(false);
   const [userData, setUserData] = useState({});
+  const [userAuthenticated, setUserAuthenticated] = useState(false);
   
   useEffect(() => {
     if (props.url) {
@@ -42,7 +77,8 @@ function View(props) {
         .then(res => res.json())
         .then(data => setUserData(data))
     } else {
-        setUserData(props.userdata);
+      setUserData(props.userdata);
+      setUserAuthenticated(true);
     }
   }, [props.url, props.userdata]);
   
@@ -56,6 +92,7 @@ function View(props) {
         show={modalShow}
         onHide={() => setModalShow(false)}
         userdata={userData}
+        isauthenticated={userAuthenticated ? 'Authenticated' : ''}
       />
     </span>
   );
